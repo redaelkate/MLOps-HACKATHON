3194766b172a87015e6a2be9da48687b961992cc
Hamza-Bouali <176033415+Hamza-Bouali@users.noreply.github.com>
2024-11-23 18:55:54 +0000

    navbar first

---
+++ MLOPS/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import NavBar from './navBar/navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+      <NavBar /> 
+    </>
+  )
+}
+
+export default App
