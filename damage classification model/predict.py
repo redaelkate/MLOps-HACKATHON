b02425160146f8c07267c416a3c4f282d4146269
EL-KATE REDA <146549050+redaelkate@users.noreply.github.com>
2024-11-24 23:16:39 +0100

    Update predict.py
    bla matzido hadchi

--- damage classification model/predict.py
+++ damage classification model/predict.py
@@ -7,19 +7,19 @@ def run_script(script_name, *args):
 def main(args):
     # Run localization scripts
     run_script("predict34_loc.py", *args)
-    run_script("predict50_loc.py", *args)
-    run_script("predict92_loc.py", *args)
-    run_script("predict154_loc.py", *args)
+    # run_script("predict50_loc.py", *args)
+    # run_script("predict92_loc.py", *args)
+    # run_script("predict154_loc.py", *args)
 
     # Run classification scripts
-    for i in range(3):
-        run_script("predict34cls.py", str(i), *args)
-        run_script("predict50cls.py", str(i), *args)
-        run_script("predict92cls.py", str(i), *args)
-        run_script("predict154cls.py", str(i), *args)
+    # for i in range(3):
+    #     run_script("predict34cls.py", str(i), *args)
+    #     run_script("predict50cls.py", str(i), *args)
+    #     run_script("predict92cls.py", str(i), *args)
+    #     run_script("predict154cls.py", str(i), *args)
 
-    # Create submission
-    run_script("create_submission.py", *args)
+    # # Create submission
+    # run_script("create_submission.py", *args)
     print("submission created!")
 
 if __name__ == "__main__":
